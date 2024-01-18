63a20041044ba7f6eae6716b9a502badd31237bb
Claudia M Cardona H <80611384+claumcarh@users.noreply.github.com>
2024-01-17 20:51:52 -0500

    se adiciona funcionalidad de buscar usuarios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,66 @@
 let tarjetas = document.getElementById('js-tarjetas');
 
+/**
+ * Crea todos los elementos de la lista no ordenada de usuarios
+ */
+function crearTarjetas(usuarios) {
+  tarjetas.innerHTML = null;
+
+  for (let index = 0; index < usuarios.length; index++) {
+    const usuario = usuarios[index];
+
+    let elTarjeta = document.createElement('li');
+    elTarjeta.classList.add('tarjetas__elemento');
+
+    let elUsuario = document.createElement('article');
+    elUsuario.classList.add('tarjeta');
+
+    let elFotoUsuario = document.createElement('img');
+    elFotoUsuario.classList.add('tarjeta__img');
+    elFotoUsuario.src = usuario.picture.medium;
+
+    let elCuerpo = document.createElement('div');
+    elCuerpo.classList.add('tarjeta__cuerpo');
+
+    let elTitulo = document.createElement('p');
+    elTitulo.classList.add('tarjeta__titulo');
+    elTitulo.innerText = `${usuario.name.first} ${usuario.name.last}`;
+
+    let elPestana = document.createElement('p');
+    elPestana.classList.add('tarjeta__pestana');
+    elPestana.innerText = usuario.gender;
+
+    let elMedio = document.createElement('p');
+    elMedio.classList.add('tarjeta__medio');
+
+    let elInfo1 = document.createElement('span');
+    elInfo1.innerText = `${usuario.location.street.number} ${usuario.location.street.name}`;
+
+    let elInfo2 = document.createElement('span');
+    elInfo2.innerText = `${usuario.location.city}, ${usuario.location.state} ${usuario.location.postcode}`;
+
+    let elInfo3 = document.createElement('span');
+    elInfo3.innerText = usuario.location.country;
+
+    let elPie = document.createElement('a');
+    elPie.classList.add('tarjeta__pie');
+    elPie.href = `mailto:${usuario.email}`;
+    elPie.innerText = usuario.email;
+
+    elMedio.appendChild(elInfo1);
+    elMedio.appendChild(elInfo2);
+    elMedio.appendChild(elInfo3);
+    elCuerpo.appendChild(elTitulo);
+    elCuerpo.appendChild(elPestana);
+    elCuerpo.appendChild(elMedio);
+    elCuerpo.appendChild(elPie);
+    elUsuario.appendChild(elFotoUsuario);
+    elUsuario.appendChild(elCuerpo);
+    elTarjeta.appendChild(elUsuario);
+    tarjetas.appendChild(elTarjeta);
+  }
+}
+
 // Obtener usuarios cuando todos los elementos y recursos estén cargados
 window.addEventListener('load', async () => {
   // Solo incluir información no sensible en la lista de usuarios
@@ -9,60 +70,28 @@ window.addEventListener('load', async () => {
 
   if (respuesta.ok) {
     let usuarios = await respuesta.json();
-    tarjetas.dataset.usuarios = usuarios;
+    tarjetas.dataset.usuarios = JSON.stringify(usuarios.results);
 
-    for (let index = 0; index < usuarios.results.length; index++) {
-      const usuario = usuarios.results[index];
+    crearTarjetas(usuarios.results);
+  }
+});
 
-      let elTarjeta = document.createElement('li');
-      elTarjeta.classList.add('tarjetas__elemento');
+let buscarBoton = document.getElementById('js-buscar-boton');
 
-      let elUsuario = document.createElement('article');
-      elUsuario.classList.add('tarjeta');
+buscarBoton.addEventListener('click', () => {
+  let buscarEntrada = document.getElementById('js-buscar-entrada');
+  let textoBuscar = buscarEntrada.value;
+  let usuarios = JSON.parse(tarjetas.dataset.usuarios);
 
-      let elFotoUsuario = document.createElement('img');
-      elFotoUsuario.classList.add('tarjeta__img');
-      elFotoUsuario.src = usuario.picture.medium;
+  // Se filtra solo si la entrada tiene texto, de lo contrario se refresca la lista con todos los usuario
+  if (textoBuscar !== null) {
+    // Filtra los usuarios obtenidos en el API teniendo en cuenta el nombre escrito
+    let usuariosFiltrados = usuarios.filter(usuario => `${usuario.name.first} ${usuario.name.last}`.includes(textoBuscar));
 
-      let elCuerpo = document.createElement('div');
-      elCuerpo.classList.add('tarjeta__cuerpo');
-
-      let elTitulo = document.createElement('p');
-      elTitulo.classList.add('tarjeta__titulo');
-      elTitulo.innerText = `${usuario.name.first} ${usuario.name.last}`;
-
-      let elPestana = document.createElement('p');
-      elPestana.classList.add('tarjeta__pestana');
-      elPestana.innerText = usuario.gender;
-
-      let elMedio = document.createElement('p');
-      elMedio.classList.add('tarjeta__medio');
-
-      let elInfo1 = document.createElement('span');
-      elInfo1.innerText = `${usuario.location.street.number} ${usuario.location.street.name}`;
-
-      let elInfo2 = document.createElement('span');
-      elInfo2.innerText = `${usuario.location.city}, ${usuario.location.state} ${usuario.location.postcode}`;
-
-      let elInfo3 = document.createElement('span');
-      elInfo3.innerText = usuario.location.country;
-
-      let elPie = document.createElement('a');
-      elPie.classList.add('tarjeta__pie');
-      elPie.href = `mailto:${usuario.email}`;
-      elPie.innerText = usuario.email;
-
-      elMedio.appendChild(elInfo1);
-      elMedio.appendChild(elInfo2);
-      elMedio.appendChild(elInfo3);
-      elCuerpo.appendChild(elTitulo);
-      elCuerpo.appendChild(elPestana);
-      elCuerpo.appendChild(elMedio);
-      elCuerpo.appendChild(elPie);
-      elUsuario.appendChild(elFotoUsuario);
-      elUsuario.appendChild(elCuerpo);
-      elTarjeta.appendChild(elUsuario);
-      tarjetas.appendChild(elTarjeta);
+    if  (usuariosFiltrados.length > 0) {
+      crearTarjetas(usuariosFiltrados);
     }
+  } else {
+    crearTarjetas(usuarios);
   }
 });
